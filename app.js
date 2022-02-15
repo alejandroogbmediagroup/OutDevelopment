2d6a6ab50a1c69bb9b1f2805b77162d86a252aa6
alejandroogbmediagroup <80288763+alejandroogbmediagroup@users.noreply.github.com>
2022-02-15 15:00:54 -0500

    keep going vue

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const app = Vue.createApp({
-    template: `
-        <h1>Hola Mundo</h1>
-        <p>Desde app.js</p>
-    `
+    // template: `
+    //     <h1>Hola Mundo</h1>
+    //     <p>Desde app.js</p>
+    // `
+    data() {
+        return {
+            quote: "Im Batman",
+            message: 'Dataaaa!!!!',
+            author: 'D10'
+        }
+    },
 })
 
 app.mount('#myApp')
