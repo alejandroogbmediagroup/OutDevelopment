fffd637dee6d11a04c5b562d31334196e606e855
alejandroogbmediagroup <80288763+alejandroogbmediagroup@users.noreply.github.com>
2022-02-16 11:18:24 -0500

    vue and css

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = Vue.createApp({
         return {
             name: 'Bitcoin',
             img: 'src/img/bitcoin-btc-logo.png',
-            changePercent: 0,
+            changePercent: 10,
+            price: 8400,
+            color: 'f4f4f4',
             //prices: [8400, 7900, 8200, 9000, 10000, 10200, 7500],
             pricesWithDays: [
                 { day: 'Monday', value: 8400 }, { day: 'Tuesday', value: 7900 }, { day: 'Wednesday', value: 8200 }, { day: 'Thursday', value: 9000 }, { day: 'Friday', value: 10000 }, { day: 'Saturday', value: 10200 }, { day: 'Sunday', value: 7500 }
@@ -20,6 +22,8 @@ const app = Vue.createApp({
     methods: {
         toggleShowPrices() {
             this.showPrices = !this.showPrices
+
+            this.color = this.color.split('').reverse().join('')
         }
     }
 })
