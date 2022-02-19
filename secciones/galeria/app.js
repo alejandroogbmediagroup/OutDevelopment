4859ecc16b48c98aa3b1764fb7ad873cff2c1618
alejandroogbmediagroup <80288763+alejandroogbmediagroup@users.noreply.github.com>
2022-02-19 15:46:00 -0500

    keep going css and gradients

---
+++ secciones/galeria/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    // template: `
+    //     <h1>Hola Mundo</h1>
+    //     <p>Desde app.js</p>
+    // `
+
+    data() {
+        return {
+            title: "Listado de Cursos de Platzi"
+        }
+    },
+
+    computed: {
+
+    },
+
+    watch: {
+
+    },
+
+    methods: {
+
+    }
+})
+app.mount('#myApp')
